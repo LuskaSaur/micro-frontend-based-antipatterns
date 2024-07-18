1e0250a89f1a17792f6a5574e9e0b5f336aab063
LuskaSaur <lucas.fernandes@meliuz.com.br>
2024-07-18 16:21:20 -0400

    feat: Add testing files

--- src/antipatterns/index.ts
+++ src/antipatterns/index.ts
@@ -1,5 +1,6 @@
 import text from './tex.json';
 import text2 from './tex2.json';
 import text3 from './tex3.json';
+import text4 from './tex4.json';
 
-export default [text, text2, text3];
+export default [text, text2, text3, text4];
