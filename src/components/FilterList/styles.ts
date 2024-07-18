dbf607f4cb8f60aa1f5724db8dde7e8c26a4ffa7
LuskaSaur <lucas.fernandes@meliuz.com.br>
2024-07-18 11:19:39 -0400

    feat: Add filter list component

---
+++ src/components/FilterList/styles.ts
@@ -0,0 +1,28 @@
+import { Button, List } from '@chakra-ui/react';
+import styled from 'styled-components';
+
+export const FilterButton = styled(Button).attrs({
+  background: 'transparent',
+  _active: {
+    bg: 'primary-active',
+    transform: 'scale(0.98)',
+  },
+  border: '1px',
+  borderRadius: '40px',
+  _hover: { bg: 'primary-hover' },
+  textAlign: 'left',
+  justifyContent: 'left',
+  justifyItems: 'center',
+  size: 'sm',
+})``;
+
+export const FilterListContainer = styled(List).attrs({
+  marginLeft: '24px',
+  marginRight: '24px',
+  marginBottom: '24px',
+  display: 'flex',
+  overflowY: 'hidden',
+  whiteSpace: 'nowrap',
+  listStyleType: 'none',
+  flexWrap: 'nowrap',
+})``;
